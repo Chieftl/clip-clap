c6f6d567f53c1b39cd6c1207fc40c067f5d17a28
Yuriy Ilkiv <ilkiv.yuriy@gmail.com>
2018-10-15 17:04:34 +0300

    Add OpenStreetMap opening_hours converter

--- src/renderer/assets/js/converter.js
+++ src/renderer/assets/js/converter.js
@@ -1,7 +1,26 @@
 let convertRules = {
-  'plus': {
-    'restriction': text => text.match(/\d+\+\d+/g),
-    'convert': text => text.replace(/(\d+)\+(\d+)/g, (match, val1, val2) => parseInt(val1) + parseInt(val2)),
+  'opening_hours': {
+    'restriction': text => text.match(/(mf|ms)/gm),
+    'convert': val => {
+      //mf1018u1220
+      //ms1018 a 1119 u1220
+      //mf10131418u1220
+      //mf8181314u920
+      val = val.toLowerCase()
+      val = val.replace(/[^a-z0-9]/g, '')
+      val = val.replace(/([^s])a/g, '$1Sa')
+      val = val.replace(/([^s])u/g, '$1Su')
+      val = val.replace('SaSu', 'Sa,Su')
+      val = val.replace('mf', 'Mo-Fr')
+      val = val.replace('ms', 'Mo-Sa')
+      val = val.replace(/(\d{2})(1[2-5])(1[2-6])([^\d]|$)/g, '$2$3$1$4')
+      val = val.replace(/(\d{1,2})(\d{2})(\d{2})(\d{2})/g, '$1:00-$2:00,$3:00-$4:00')
+      val = val.replace(/(\d{1,2})(\d{2})/g, '$1:00-$2:00')
+      val = val.replace(/(\d)([a-z])/ig, '$1; $2')
+      val = val.replace(/([a-z])(\d)/ig, '$1 $2')
+      val = val.replace(/([^\d])([3-9])/g, '$10$2')
+      return val
+    },
   },
 }
 
